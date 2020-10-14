2a013ba52a5d77b6d15ffb1d0cae8ad00951a4e0
halim <haleemyoosuph@gmail.com>
2020-10-14 03:14:00 +0100

    chore: import and use of icon containers

--- components/QuickAccess.jsx
+++ components/QuickAccess.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import IconCont from './AccessIconContainer';
+import { BsMusicNoteList, BsDownload, BsImages } from 'react-icons/bs';
+import { FiVideo } from 'react-icons/fi';
+import { IoMdAppstore } from 'react-icons/io';
+import { CgFileDocument } from 'react-icons/cg';
 
 const QuickAccess = () => {
-  return <div className='text-lg'>Quick Access</div>;
+  const generateRandomColor = () => {};
+  return (
+    <div>
+      <div className='text-lg pb-4'>Quick Access</div>
+      <div className='flex flex-wrap justify-space-between'>
+        <div className='px-3 py-2'>
+          <IconCont color='#1976D2'>
+            <div>
+              <BsMusicNoteList size='20' color='#fff' />
+            </div>
+          </IconCont>
+        </div>
+        <div className='px-3 py-2'>
+          <IconCont color='#1976D2'>
+            <div>
+              <FiVideo size='20' color='#fff' />
+            </div>
+          </IconCont>
+        </div>
+        <div className='px-3 py-2'>
+          <IconCont color='#1976D2'>
+            <div>
+              <CgFileDocument size='20' color='#fff' />
+            </div>
+          </IconCont>
+        </div>
+        <div className='px-3 py-2'>
+          <IconCont color='#1976D2'>
+            <div>
+              <BsDownload size='20' color='#fff' />
+            </div>
+          </IconCont>
+        </div>
+        <div className='px-3 py-2'>
+          <IconCont color='#1976D2'>
+            <div>
+              <IoMdAppstore size='20' color='#fff' />
+            </div>
+          </IconCont>
+        </div>
+        <div className='px-3 py-2'>
+          <IconCont color='#1976D2'>
+            <div>
+              <BsImages size='20' color='#fff' />
+            </div>
+          </IconCont>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QuickAccess;
