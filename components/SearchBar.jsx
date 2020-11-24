679e06cff93a37e75ab68d513a3733e8ef592fbd
halim <haleemyoosuph@gmail.com>
2020-11-24 14:03:44 +0100

    chore: add icons to link sidenavs

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import styles from '../styles/search.module.css';
-import { BiSearchAlt } from 'react-icons/bi';
+import { BiSearchAlt, BiCaretRight, BiArrowToRight } from 'react-icons/bi';
+import { BsDownload } from 'react-icons/bs';
 const SearchBar = () => {
   return (
-    <>
-      <div className={`${styles.searchIcon}`}>
-        <BiSearchAlt color='grey' size='20' />
+    <div className={styles.barCont}>
+      <div className='px-3'>
+        <BiCaretRight color='#1976D2' size='30' />
       </div>
+      {/* <div className={`${styles.searchIcon}`}>
+        <BiSearchAlt color='grey' size='25' />
+      </div> */}
       <input
-        className={`${styles.searchInput} text-sm px-8 py-1`}
+        className={`${styles.searchInput} text-sm px-8 py-3`}
         placeholder='Type here...'
       />
-    </>
+      <div className='px-3'>
+        <BsDownload color='#1976D2' size='30' />
+      </div>
+    </div>
   );
 };
 
