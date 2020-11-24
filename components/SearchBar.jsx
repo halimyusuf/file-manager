b97e2d456a1efedbc41a4f5cfc7d4e84d741f50d
halim <haleemyoosuph@gmail.com>
2020-11-24 16:08:18 +0100

    fixed search bar styles

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import styles from '../styles/search.module.css';
 import { BiSearchAlt, BiCaretRight, BiArrowToRight } from 'react-icons/bi';
+import { VscMenu } from 'react-icons/vsc';
+import { GoCloudDownload } from 'react-icons/go';
 import { BsDownload } from 'react-icons/bs';
 const SearchBar = () => {
   return (
     <div className={styles.barCont}>
-      <div className='px-3'>
-        <BiCaretRight color='#1976D2' size='30' />
+      <div className='hide-lg pr-4'>
+        <VscMenu size='25' />
       </div>
       {/* <div className={`${styles.searchIcon}`}>
         <BiSearchAlt color='grey' size='25' />
       </div> */}
+
       <input
-        className={`${styles.searchInput} text-sm px-8 py-3`}
+        className={`${styles.searchInput} text-sm px-4 py-2`}
         placeholder='Type here...'
       />
-      <div className='px-3'>
-        <BsDownload color='#1976D2' size='30' />
+      <div className='pl-3'>
+        <GoCloudDownload color='#1976D2' size='25' />
       </div>
     </div>
   );
